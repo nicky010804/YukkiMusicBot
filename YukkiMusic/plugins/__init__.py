c0883240aaaa06085a1605e4a8601e0a62944878
nicky010804 <104410019+nicky010804@users.noreply.github.com>
2022-07-14 20:13:15 +0900

    __init__.py

--- YukkiMusic/plugins/__init__.py
+++ YukkiMusic/plugins/__init__.py
@@ -14,10 +14,10 @@ from os.path import dirname, isfile
 
 def __list_all_modules():
     work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+    mod_paths = glob.glob(work_dir + "!*!*.py")
 
     all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
+        (((f.replace(work_dir, "")).replace("!", "!"))[:-3])
         for f in mod_paths
         if isfile(f)
         and f.endswith(".py")
